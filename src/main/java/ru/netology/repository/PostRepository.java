e8a4587e6bcff1c238cb3416dcf2aad7e9da3204
abramovanv <abramov_anv@rambler.ru>
2026-03-09 08:54:40 +0300

    True commit

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -2,24 +2,44 @@ package ru.netology.repository;
 
 import ru.netology.model.Post;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.Collection;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicLong;
 
 // Stub
 public class PostRepository {
-  public List<Post> all() {
-    return Collections.emptyList();
+
+  private final ConcurrentMap<Long, Post> allPosts;
+  private final AtomicLong idCounter = new AtomicLong();
+
+  public PostRepository() {
+    this.allPosts = new ConcurrentHashMap<>();
+  }
+
+  public Collection<Post> all() {
+    return allPosts.values();
   }
 
   public Optional<Post> getById(long id) {
-    return Optional.empty();
+    return Optional.ofNullable(allPosts.get(id));
   }
 
   public Post save(Post post) {
+    if (post.getId() == 0) {
+      long id = idCounter.incrementAndGet();
+      post.setId(id);
+      allPosts.put(id,post);
+    } else if (post.getId() != 0) {
+      Long currentId = post.getId();
+      allPosts.put(currentId, post);
+    }
     return post;
   }
 
   public void removeById(long id) {
+    allPosts.remove(id);
   }
 }
+
